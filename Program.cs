04900bd837e2dffe784fcc1f7448404d46bbd93e
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2021-12-22 17:03:56 +0300

    Switch home work

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Switch_case_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      //Case 1.
+           // int a;
+           // Console.WriteLine("Название дня недели: ");
+           // a = Convert.ToInt32(Console.ReadLine());
+           // switch (a)
+           // {
+           //     case 1:
+           //         Console.WriteLine("Понедельник");break;
+           //     case 2:
+           //         Console.WriteLine("Вторник");break;
+           //         case 3:
+           //             Console.WriteLine("Среда");break;
+           //         case 4: 
+           //             Console.WriteLine("Четверг");break;
+           //         
+           //         case  5: 
+           //             Console.WriteLine("Пятница");
+           //             break;
+           //         case  6:
+           //             Console.WriteLine("Суббота");break;
+           //         case 7:
+           //             Console.WriteLine("Воскресенье");
+           //             break;
+           //
+           //
+           //
+           //
+           // }
+
+        // Case2:
+
+        // int kNumber;
+        // Console.WriteLine("Оценки: ");
+        // kNumber = Convert.ToInt32(Console.ReadLine());
+        // switch (kNumber)
+        // {
+        //     case 1: 
+        //         Console.WriteLine("Плохо");break;
+        //     case  2: 
+        //         Console.WriteLine("Неудовлетворительно");break;
+        //     case  3: 
+        //         Console.WriteLine("Удовлетворительно");break;
+        //     case  4: 
+        //         Console.WriteLine("Хорошо");break;
+        //     case 5:
+        //         Console.WriteLine("Отлично");break;
+        //     default:
+        //         Console.WriteLine("Wrong");break;
+        // }
+        //
+    
+        //Case 3
+
+        // int monthNumber;
+        // Console.WriteLine("Месяц: ");
+        // monthNumber = Convert.ToInt32(Console.ReadLine());
+        // switch (monthNumber)
+        // {
+        //     case  1:
+        //         Console.WriteLine("Зима");break;
+        //     case  2:
+        //         Console.WriteLine("Весна");break;
+        //     case 3:
+        //         Console.WriteLine("Лето");break;
+        //     case 4:
+        //         Console.WriteLine("Осень");break;
+        // }
+        
+        //Case 4.
+        //
+        // int integerNumber;
+        // Console.WriteLine("Високоносный год: ");
+        // integerNumber = Convert.ToInt32(Console.ReadLine());
+        // integerNumber /= 4;
+        // switch (integerNumber)
+        // {
+        //     case 1:
+        //         Console.WriteLine("Январь");break;
+        //         
+        //         case 2:
+        //             Console.WriteLine("Февраль");break;
+        //         case 3:
+        //             Console.WriteLine("Март");
+        //     break;    
+        //     case 4:
+        //             Console.WriteLine("Апрель");
+        //             break;
+        //         case 5:Console.WriteLine("Май");break;
+        //         
+        //         case 6: Console.WriteLine("Июнь");break;
+        //         case 7:Console.WriteLine("Июль");
+        //             break;
+        //         case 8:Console.WriteLine("Август");break;
+        //         case 9:Console.WriteLine("Сентябрь");
+        //             break;
+        //         
+        //         case 10:Console.WriteLine("Октябрь");
+        //             break;
+        //         case 11:Console.WriteLine("Ноябрь");break;
+        //         case 12:Console.WriteLine("Декабрь");break;
+        // }
+        //
+
+       //Case 5.
+//?
+       // int number_1 = Convert.ToInt32(Console.ReadLine());
+       // int number_2 = Convert.ToInt32(Console.ReadLine());
+       // Console.WriteLine(number_1);
+       // Console.WriteLine(number_2);
+       // switch (number_1  && number_2 != 0)
+       // {
+       //     case 1:
+       //         Console.WriteLine("Сложение");break;
+       //     case  2:
+       // }
+
+        }
+    }
+}
